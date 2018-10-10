e3235a29eb08d098987df0c3c3055336b7aaba8d
victor dias <victor.dias@meumobi.com>
2018-10-11 00:36:00 +0100

    feature(api): add api service (core module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    CoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ApiService } from '@core/services/api.service';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    ApiService
+  ]
 })
 export class CoreModule { }
